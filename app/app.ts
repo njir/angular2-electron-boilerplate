1b7ffc8fe6f94b8e888f52f04986f55996e7764d
Hyung Tak Jin <njir@naver.com>
2016-04-10 10:35:48 +0000

    refactor: Modify component

--- app/app.ts
+++ app/app.ts
@@ -18,71 +18,15 @@ class ByteFormatPipe implements PipeTransform {
   selector: 'app',
   pipes: [ByteFormatPipe],
   template: `
-    <h1>Total Images: {{ imageStats().count }}</h1>
-    <h1>Total Size: {{ imageStats().size | byteFormat}}</h1>
-    <div 
-      (dragover)="false" 
-      (dragend)="false" 
-      (drop)="handleDrop($event)"
-      style="height: 300px; border: 5px dotted #ccc;">
-      <p style="margin: 10px; text-align: center">
-        <strong>Drop Your Images Here</strong>
-      </p>
-    </div>
-    <div class="media" *ngFor="#image of images">
-      <div class="media-left">
-        <a href="#">
-          <img class="media-object" src="{{image.path}}" style="max-width:200px">
-        </a>
-      </div>
-      <div class="media-body">
-        <h4 class="media-heading">{{image.name}}</h4>
-        <p>{{image.size | byteFormat}}</p>
-      </div>
-    </div>
+   <div class="container">
+        <h1>Hello Electron</h1>
+  </div>
   ` 
 })
 
 export class App {
-
-  images:Array<Object> = [];
-
   constructor() {}
-
-  handleDrop(e) {
-    var files:File = e.dataTransfer.files;
-    var self = this;
-    Object.keys(files).forEach((key) => {
-      if(files[key].type === "image/png" || files[key].type === "image/jpeg") {
-        self.images.push(files[key]);
-      }
-      else {
-        alert("File must be a PNG or JPEG!");
-      }
-    });
-
-    return false;
   }
-
-  imageStats() {
-
-    let sizes:Array<number> = [];
-    let totalSize:number = 0;
-    
-    this
-      .images
-      .forEach((image:File) => sizes.push(image.size));
-
-    sizes
-      .forEach((size:number) => totalSize += size);
-
-    return {
-      size: totalSize,
-      count: this.images.length
-    }
-    
-  }
-
 }
 
 bootstrap(App);
